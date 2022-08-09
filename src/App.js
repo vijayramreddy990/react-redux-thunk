480e94964632d525fd3a9d3a57925be9c00b4b92
vijay <vijayramreddy990@gmail.com>
2022-08-09 18:29:40 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Header from "./containers/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<ProductListing />} />
+          <Route path="/product/:productId" exact element={<ProductDetail />} />
+          {/* <Route path="*" element={<>} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 };
